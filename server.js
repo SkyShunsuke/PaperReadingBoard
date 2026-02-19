1bf6c02868a6e1f9258aac5edeab9a89a5538ec4
TSTB-dev <s223107@gm.ishikawa-nct.ac.jp>
2026-02-19 20:57:04 +0900

    update for railway serving

--- server.js
+++ server.js
@@ -205,9 +205,12 @@ app.get("*", (_req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => console.log("listening on", port));
 
 function publicState() {
   return {
